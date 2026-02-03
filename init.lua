0e8dfcfefbb9e94b8f10d37a264fd18fa1b3fe37
Neli Harbuzava <ineliharbuzava@gmail.com>
2026-02-03 13:39:22 +0300

    highlight yanked text

--- init.lua
+++ init.lua
@@ -23,3 +23,12 @@ end, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>p', function ()
 	vim.fn.setreg('+', vim.fn.expand('%:p'))
 end, { desc = 'Copy Current Buffer Path' })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function() vim.hl.on_yank() end,
+})
