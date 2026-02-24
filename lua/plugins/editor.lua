e89790eec99c5e50707011a45e3d009f86f3e3fb
Neli Harbuzava <ineliharbuzava@gmail.com>
2026-02-24 11:26:42 +0300

    Highlight TODO comments

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,19 @@
 return {
 	-- Show parent function / method on top of the editor
-	{ "nvim-treesitter/nvim-treesitter-context", version = "1.0.0" }
+	{ "nvim-treesitter/nvim-treesitter-context", version = "1.0.0" },
+	-- Highlight todo, notes, etc in comments
+	{
+		'folke/todo-comments.nvim',
+		version = "1.5.0",
+		event = 'VimEnter',
+		dependencies = {
+			'nvim-lua/plenary.nvim'
+		},
+		opts = {
+			signs = false,
+			keywords = {
+				TODO = { color = "warning" },
+  			},
+		}
+	},
 }
